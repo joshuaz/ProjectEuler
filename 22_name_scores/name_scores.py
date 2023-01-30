0f19baf8185988144813b4524e14e77931379320
joshuaz <joshuaz@users.noreply.github.com>
2023-01-29 19:14:11 -0500

    Update name_scores.py

--- 22_name_scores/name_scores.py
+++ 22_name_scores/name_scores.py
@@ -13,3 +13,15 @@ def nameScores(arr):
         total_score += index * sum(alphabet.find(c)+1 for c in line)
     
     return(total_score)
+
+
+#from string import ascii_uppercase as upper
+#total_score = 0
+#data = urlopen('https://projecteuler.net/project/resources/p022_names.txt')
+#txt = data.read()
+#lines = str(txt).split(",")  # Opening the file and assigning the string to "lines"
+#L1 = sorted([_.strip('"') for _ in lines]) # Removes the individual quotes and sorts the list as well
+#for i in L1:
+#    letter_score = sum([upper.find(j)+1 for j in i]) # Sums up the index of individual letters in the words so as to get letter score
+#    total_score += letter_score*(L1.index(i)+1)
+#print(total_score)
