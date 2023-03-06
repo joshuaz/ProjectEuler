efb59715094ef22c4928369bbcad62fb108614f2
joshuaz <joshuaz@users.noreply.github.com>
2023-03-05 19:15:13 -0500

    Update test_module.py

--- 23_non_abundant_sums/test_module.py
+++ 23_non_abundant_sums/test_module.py
@@ -5,27 +5,27 @@ import unittest
 class UnitTests(unittest.TestCase):
 
     def test1(self):
-        actual = type(sumOfNonAbundantNumbers(10000))
+        actual = sumOfNonAbundantNumbers(10000)
         expected = 3731004
         self.assertEqual(type(actual), type(expected), 'Expected an int')
 
     def test2(self):
-        actual = type(sumOfNonAbundantNumbers(10000))
+        actual = sumOfNonAbundantNumbers(10000)
         expected = 3731004
         self.assertEqual(actual, expected, 'Expected an int')
 
     def test3(self):
-        actual = type(sumOfNonAbundantNumbers(15000))
+        actual = sumOfNonAbundantNumbers(15000)
         expected = 4039939
         self.assertEqual(actual, expected, 'Expected an int')
 
     def test4(self):
-        actual = type(sumOfNonAbundantNumbers(20000))
+        actual = sumOfNonAbundantNumbers(20000)
         expected = 4159710
         self.assertEqual(actual, expected, 'Expected an int')
 
     def test5(self):
-        actual = type(sumOfNonAbundantNumbers(28123))
+        actual = sumOfNonAbundantNumbers(28123)
         expected = 4179871
         self.assertEqual(actual, expected, 'Expected an int')
     
